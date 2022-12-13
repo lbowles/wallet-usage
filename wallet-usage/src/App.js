80fcbb85b0506d5fa597233a04882df9eb168932
lbowles <lspbowles@gmail.com>
2022-12-13 17:13:13 +0200

    split up tx by months

--- wallet-usage/src/App.js
+++ wallet-usage/src/App.js
@@ -48,22 +48,6 @@ function App() {
     console.log(transactions)
   }
 
-  const getChartDate = () => {
-    const startDate = new Date('2021-01-22')
-    const endDate = new Date('2021-01-27')
-
-    const filteredTransactions = transactions.filter((tx) => {
-      var date = new Date(tx.timestamp * 1000)
-      if (date >= startDate && date <= endDate) {
-        return tx
-      }
-    })
-
-    console.log(startDate)
-    console.log(filteredTransactions)
-    console.log(transactions[0].timestamp)
-  }
-
   return (
     <div className="bg-slate-900">
       <Web3Modal
@@ -73,16 +57,16 @@ function App() {
       />
       {/* //TODO add ENS support */}
       <WagmiConfig client={wagmiClient}>
-        <div className="flex justify-center space-x-4 pt-11 ">
-          <h1 class="text-7xl subpixel-antialiased font-normal tracking-wide text-slate-100">
+        <div className="flex justify-center space-x-4 pt-20 pb-10">
+          <h1 className="text-7xl subpixel-antialiased font-normal tracking-wide text-slate-100">
             Wallet Usage
           </h1>
           <img src={logo}></img>
         </div>
         <div className="flex flex-row justify-center space-x-4 pt-11">
-          <form class="group relative w-80">
+          <form className="group relative w-80">
             <input
-              class="bg-slate-800 focus:ring-2 focus:#794DFF focus:outline-none appearance-none w-full text-sm leading-6 text-white placeholder-slate-200 rounded-md py-2 pl-2 ring-1 ring-slate-700 shadow-sm disabled:bg-slate-700"
+              className="bg-slate-800 focus:ring-2 focus:#794DFF focus:outline-none appearance-none w-full text-sm leading-6 text-white placeholder-slate-200 rounded-md py-2 pl-2 ring-1 ring-slate-700 shadow-sm disabled:bg-slate-700"
               type="text"
               aria-label="Search Address"
               placeholder="Search Address.."
@@ -91,9 +75,8 @@ function App() {
           </form>
           <Web3Button />
         </div>
-        <Chart></Chart>
+        <Chart transactions={transactions}></Chart>
         <button onClick={() => getTxHistory()}>get history</button>
-        <button onClick={() => getChartDate()}>get chart date</button>
       </WagmiConfig>
     </div>
   )
--- wallet-usage/src/ChartHistory.js
+++ wallet-usage/src/ChartHistory.js
@@ -1,43 +1,123 @@
-import { Chart } from 'chart.js/auto'
-;(async function () {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ]
+import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2'
 
-  new Chart(document.getElementById('myChart'), {
-    type: 'bar',
-    data: {
-      labels: data.map((row) => row.year),
-      datasets: [
-        {
-          label: 'Acquisitions by year',
-          data: data.map((row) => row.count),
-        },
-      ],
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
     },
-  })
-})()
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+}
+const mokData = [
+  { count: 10 },
+  { count: 20 },
+  { count: 15 },
+  { count: 25 },
+  { count: 22 },
+  { count: 30 },
+  { count: 10 },
+  { count: 10 },
+  { count: 20 },
+  { count: 15 },
+  { count: 25 },
+  { count: 25 },
+]
 
-export default function ChartHistory(transactions) {
+const labels = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+]
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: mokData.map((row) => row.count),
+      backgroundColor: '#A978C4',
+    },
+  ],
+}
+
+export default function ChartHistory({ transactions }) {
+  var txByMonth = new Map()
+
+  const getChartDate = () => {
+    const startDate = new Date('2020-01-22')
+    const endDate = new Date('2022-01-27')
+
+    const filteredTransactions = transactions.filter((tx) => {
+      var date = new Date(tx.timestamp * 1000)
+      if (date >= startDate && date <= endDate) {
+        return tx
+      }
+    })
+
+    console.log(startDate)
+    console.log(filteredTransactions)
+    console.log(transactions[0].timestamp)
+
+    // Create a new Map to store the events for each month.
+
+    // Loop through the events and add them to the Map.
+    for (var event of filteredTransactions) {
+      // Get the month for the event.
+      var date = new Date(event.timestamp * 1000)
+      var month = date.getMonth()
+      console.log(date + '----' + month)
+      // Check if we have already added an array for this month.
+      if (!txByMonth.has(month)) {
+        txByMonth.set(month, [])
+      }
+
+      // Add the event to the array for this month.
+      txByMonth.get(month).push(event)
+    }
+
+    // Loop through the Map and print out the events for each month.
+    for (var [month, events] of txByMonth) {
+      console.log(`Month: ${month}, Events: ${events}`)
+    }
+  }
+
   return (
     <div>
       <div className="flex justify-center pt-11">
-        <div className="block p-6 rounded-lg shadow-lg bg-slate-800 ">
+        <div className="block p-6 rounded-lg shadow-lg bg-slate-800 w-full max-w-2xl ">
           <h5 className="text-gray-100 text-xl leading-tight font-medium mb-2">
             Transaction Frequency
           </h5>
           <p className="text-gray-300 text-base mb-4">
             Click a month to see a breakdown of the transactions
           </p>
-          <div style={{ width: '700px' }}>
-            <canvas id="myChart"></canvas>
-          </div>
+          <Bar options={options} data={data} />
+          <button onClick={() => getChartDate()}>get chart date</button>
         </div>
       </div>
     </div>
