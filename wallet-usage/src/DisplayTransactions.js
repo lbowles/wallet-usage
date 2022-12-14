e9d4a11a0230b822b50dc3623cd28aa4722eb1b9
lbowles <lspbowles@gmail.com>
2022-12-14 18:21:22 +0200

    Populating table with selected bar

--- wallet-usage/src/DisplayTransactions.js
+++ wallet-usage/src/DisplayTransactions.js
@@ -1,16 +1,64 @@
+import { useEffect, useState } from 'react'
+import open from './open.svg'
+
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
 export default function DisplayTransactions({ selectedMonth }) {
+  const [tableRows, setTableRows] = useState()
+
+  const getDate = (timestamp) => {
+    return new Date(timestamp * 1000).toLocaleDateString()
+  }
+
+  const getMonth = (timestamp) => {
+    const date = new Date(timestamp * 1000)
+    return monthNames[date.getMonth()] + ' ' + date.getFullYear()
+  }
+
+  useEffect(() => {
+    if (selectedMonth) {
+      setTableRows(
+        selectedMonth.map((trans) => (
+          <tr className="bg-white border-b dark:bg-slate-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <td className="py-4 px-6">{getDate(trans.timestamp)}</td>
+            <td className="py-4 px-6">{trans.hash.substring(0, 14)}...</td>
+            <td className="py-4 px-6">Laptop</td>
+            <td className="py-4 px-6">
+              <a>
+                <img src={open} style={{ height: '18px' }}></img>
+              </a>
+            </td>
+          </tr>
+        )),
+      )
+    }
+  }, [selectedMonth])
+
   return (
     <div>
       {selectedMonth && (
         <div className="flex justify-center pt-4">
           <div className="block p-6 rounded-lg shadow-lg bg-slate-800 w-full max-w-2xl ">
-            <h5 className="text-gray-100 text-xl leading-tight font-medium mb-3">
-              Feb 2022
+            <h5 className="text-gray-100 text-xl leading-tight font-medium mb-4">
+              {getMonth(selectedMonth[0].timestamp)}
             </h5>
 
-            <div className="overflow-x-auto relative  sm:rounded-sm">
+            <div className="overflow-x-auto relative  sm:rounded-sm max-h-72">
               <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 sticky top-0">
                   <tr>
                     <th scope="col" className="py-3 px-6">
                       Date
@@ -26,19 +74,7 @@ export default function DisplayTransactions({ selectedMonth }) {
                     </th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr className="bg-white border-b dark:bg-slate-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Apple MacBook Pro 17"
-                    </th>
-                    <td className="py-4 px-6">Sliver</td>
-                    <td className="py-4 px-6">Laptop</td>
-                    <td className="py-4 px-6">$2999</td>
-                  </tr>
-                </tbody>
+                <tbody>{tableRows}</tbody>
               </table>
             </div>
           </div>
