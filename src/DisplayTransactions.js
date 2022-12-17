3430e47ec0f9372a7ba42e85c01011e854db8283
lbowles <lspbowles@gmail.com>
2022-12-17 16:09:02 +0200

    Update DisplayTransactions.js

--- src/DisplayTransactions.js
+++ src/DisplayTransactions.js
@@ -95,7 +95,7 @@ export default function DisplayTransactions({ selectedMonth }) {
               {getMonth(selectedMonth[0].timeStamp)}
             </h5>
             <p className="text-gray-400 text-base mb-4">
-              {totalGasUsed} Gwei was used for gas this month
+              {totalGasUsed} Total gas used for this month
             </p>
             {loading ? (
               <div className="flex justify-center">
@@ -118,7 +118,7 @@ export default function DisplayTransactions({ selectedMonth }) {
                         Transaction Hash
                       </th>
                       <th scope="col" className="py-3 px-6">
-                        Gass Used (Gwei)
+                        Gass Used
                       </th>
                       <th scope="col" className="py-3 px-6">
                         Link
